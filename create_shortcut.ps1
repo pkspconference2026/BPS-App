88b7457e23cf46e6e372916d7c7ff7bf269b85f4
pkspconference2026 <hafiz.yahya@moh.gov.my>
2026-08-11 11:24:24 +0800

    v1.0.1: buang fail obsolete (create_shortcut.ps1, Jalan BPS.bat, app.py.bak)

--- create_shortcut.ps1
+++
@@ -1,8 +0,0 @@
-$WS = New-Object -ComObject WScript.Shell
-$SC = $WS.CreateShortcut([Environment]::GetFolderPath('Desktop') + '\BPS Report.lnk')
-$SC.TargetPath = 'C:\Users\User\Desktop\BPS-App\Jalan BPS.bat'
-$SC.WorkingDirectory = 'C:\Users\User\Desktop\BPS-App'
-$SC.Description = 'BPS Report Generator - Laporan Penilaian Biopsikososial'
-$SC.IconLocation = 'C:\Users\User\Desktop\BPS-App\BPS.ico,0'
-$SC.Save()
-Write-Host 'Shortcut updated!'
